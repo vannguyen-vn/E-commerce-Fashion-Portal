da173790e19a51ff4b457135906c476e07630f64
Van Nguyen <van.nguyen@texo.design>
2021-12-11 16:32:11 -0800

    Setup Webpack and React

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Hi, I am Van</h1>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  devtool: 'source-map',
+  // devtool: 'eval-source-map', // used to be, for development
+  entry: `${SRC_DIR}/index.jsx`,
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  }
+};
