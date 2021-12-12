8c991f6949b0b974570f0f79e5c24cb201d058c0
Van Nguyen <van.nguyen@texo.design>
2021-12-11 18:39:40 -0800

    Set up environment

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.static(__dirname + '/../dist'));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log('Listening on port: ', PORT);
+});
