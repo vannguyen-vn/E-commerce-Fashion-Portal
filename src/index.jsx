aa9d627749f7793dc847fad75adb1060c2d907d4
Van Nguyen <van.nguyen@texo.design>
2022-01-06 01:20:11 -0800

    Fixed bug for when deoloy to production

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import '../src/css/main.scss'
+import '../src/css/main.scss';
 
-ReactDOM.render(
-  <App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
