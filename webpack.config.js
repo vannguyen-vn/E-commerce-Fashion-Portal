9b2caa2251b25c18e1bb2e21e4cbf7df75d03c0d
Van Nguyen <van.nguyen@texo.design>
2022-01-05 23:57:59 -0800

    Fixed bug of webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = function (_env, argv) {
     devtool: isDevelopment && "cheap-module-source-map",
     entry: "./client/src/index.jsx",
     output: {
-      path: path.resolve(__dirname, "client/dist"),
+      path: path.resolve(__dirname, "./client/dist"),
       filename: "assets/js/[name].[contenthash:8].js",
       publicPath: "/"
     },
